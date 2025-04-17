0df74170eccf6d0d2f799c6d8181b091a27f95fa
varunreddychinthareddy09 <115725078+varunreddychinthareddy09@users.noreply.github.com>
2025-04-17 10:39:46 +0530

    Update pkg1.py

--- Day4/pkg1.py
+++ Day4/pkg1.py
@@ -1,1 +1,11 @@
+from Day4_Q1 import File
+import datetime
 
+if __name__ == "__main__":
+    fs = File(".")
+
+    max_files = fs.getMaxsizeFile(2)
+    print("Max Size Files:", max_files)
+
+    latest_files = fs.getLatestFiles(datetime.datetime(2018, 2, 1))
+    print("Latest Files:", latest_files)
